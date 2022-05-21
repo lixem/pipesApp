979bd6f73f9d0216584dbf6161798a117fee60fa
lixem <pantojakevin@gmail.com>
2022-05-20 19:52:59 -0500

    Ultimo cambio

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -9,6 +10,41 @@ import { Component, OnInit } from '@angular/core';
 export class OrdenarComponent implements OnInit {
 
   enMayusculas: boolean = true;
+
+  ordenarPor: string = '';
+
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    }
+    ,
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
@@ -16,4 +52,12 @@ export class OrdenarComponent implements OnInit {
   toogle(){
     this.enMayusculas = !this.enMayusculas;
   }
+
+
+  cambiarOrden( valor: string ){
+    this.ordenarPor = valor;
+    console.log(valor);
+  }
+
+
 }
